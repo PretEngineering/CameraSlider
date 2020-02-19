4f3f341209412d34292ac462a7bbf840cbffcec9
lnimmo <55515724+lnimmo@users.noreply.github.com>
2020-02-19 09:49:46 -0800

    direction controlled with pushbutton

--- CamerSlider.ino
+++ CamerSlider.ino
@@ -2,6 +2,14 @@
 #define STEP_PIN  6                // Step on rising edge
 #define DIR_PIN 8
 
+const int RIGHT_PIN = 9;
+const int LEFT_PIN = 10;
+
+int speed = 75;
+
+int leftButton = 0;
+int rightButton = 0;
+
 #include <TMC2208Stepper.h>             // Include library
 
 TMC2208Stepper driver = TMC2208Stepper(&Serial);  // Create driver and use
@@ -11,7 +19,13 @@ void setup() {
   Serial.begin(115200);             // Init used serial port
   while(!Serial);                 // Wait for port to be ready
 
-  // prepare pins
+//   // prepare pins
+
+pinMode(RIGHT_PIN, INPUT);
+pinMode(LEFT_PIN, INPUT);
+
+
+
   pinMode(EN_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
@@ -26,22 +40,29 @@ void setup() {
 }
 
 void loop() {
+  leftButton = digitalRead(LEFT_PIN);
+  rightButton = digitalRead(RIGHT_PIN);
 
-  digitalWrite(DIR_PIN, HIGH); // move in one direction
-  for(int x = 0; x < 4000; x++){
+if (leftButton == 1){
+    digitalWrite(DIR_PIN, HIGH); // move left
     digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(400);
+    delayMicroseconds(speed);
     digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(400);
+    delayMicroseconds(speed);
   }
 
-  digitalWrite(DIR_PIN, LOW); // move in other direction
-  for(int x = 0; x < 4000; x++){
+
+if (rightButton == 1){
+    digitalWrite(DIR_PIN, LOW); // move right
     digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(400);
+    delayMicroseconds(speed);
     digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(400);
+    delayMicroseconds(speed);
   }
 
-  delay(1000);
 }
+  // Serial.println("testing");
+  // Serial.println(leftButton);
+  // Serial.println(rightButton);
+  // Serial.println();
+
